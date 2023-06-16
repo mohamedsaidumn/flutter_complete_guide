b53e1b980deeda916f164aab8d34c6b06e691d52
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-06-16 12:05:39 -0500

    The Scaffold is a widget in Flutter used to implements the basic material design visual layout structure, i used it to set a nice background layout that this material design

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("My App"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+        ),
+        body: Text("This is the nody"),
+      ),
     );
   }
 }
