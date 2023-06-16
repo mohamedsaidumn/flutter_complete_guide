d02f8cf29e4e95f8d8a00c36c0f819cf819b1694
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-06-16 15:57:46 -0500

    make a question widget and import it

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import "./question.dart";
+
 void main() {
   runApp(MyApp());
 }
@@ -34,7 +36,7 @@ class _MyAppState extends State<MyApp> {
             title: Text("My App"),
           ),
           body: Column(children: [
-            Text(questions[_questionIndex]),
+            Question(questions[_questionIndex]),
             ElevatedButton(
               child: Text("Answer 1"),
               onPressed: _answerQuestion,
