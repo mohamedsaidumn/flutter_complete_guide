03ea69d241e5b3f0916b360c266711cf1b717804
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-06-18 15:49:07 -0500

    used container, row and column with styling to create nice transaction ui

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,48 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(
             children: transactions.map((tx) {
-              return Card(child: Text(tx.title));
+              // return Card(child: Text(tx.title));
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        tx.date.toString(),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      )
+                    ])
+                  ],
+                ),
+              );
             }).toList(),
           )
         ],
