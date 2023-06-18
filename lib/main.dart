e7a461fa1005a6fc67d62de6ca442b19548d20b0
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-06-17 20:56:50 -0500

    set up the and gave it appBAr and body with just text

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,22 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("My App"),
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(
+        child: Text('Widget Playground!'),
+      ),
     );
   }
 }
